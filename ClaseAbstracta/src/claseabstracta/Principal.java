fc10410f895dc7aa497a77ad5ef41adb5189465c
Tomás Puchana <109983088+Tpuchana@users.noreply.github.com>
2023-05-30 09:12:38 -0500

    1

--- ClaseAbstracta/src/claseabstracta/Principal.java
+++ ClaseAbstracta/src/claseabstracta/Principal.java
@@ -1,44 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package claseabstracta;
 
-/**
- *
- * @author SALA
- */
+import java.util.ArrayList;
+
 public class Principal {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ArrayList<medioTransporte> ListaVehiculos  = new ArrayList<>();
+        ListaVehiculos.add(new transporteTerrestre(categoriaTTerrestre.AUTO, 4, true, 5));
+        ListaVehiculos.add(new Avion(4,100)));
+        ListaVehiculos.add(new transporteTerrestre(categoriaTTerrestre.AUTO, 4, true, 5));
+        ListaVehiculos.add(new transporteTerrestre(categoriaTTerrestre.AUTO, 4, true, 5));
+        ListaVehiculos.add(new transporteTerrestre(categoriaTTerrestre.AUTO, 4, true, 5));
         
-        transporteTerrestre auto1 = new transporteTerrestre(categoriaTTerrestre.AUTO, 4,true,5);
+        transporteTerrestre moto = ListaVehivulos.get
         
-        //medioTransporte m1 = new medioTransporte(true,8); -> las clases abstractas no pueden crear objetos
-      
-        auto1.encender();
-        auto1.acelerar();
-        auto1.conducir();
-        auto1.frenar();
-        auto1.apagar();
-        
-        transporteTerrestre bici1 = new transporteTerrestre(categoriaTTerrestre.BICICLETA, 2, false, 2);
-        bici1.encender();
-        bici1.acelerar();
-        bici1.conducir();
-        bici1.frenar();
-        bici1.apagar();
-        
-        Avion avion1 = new Avion(2, 30);
-        avion1.encender();
-        avion1.acelerar();
-        avion1.despegar();
-        avion1.frenar();
-        avion1.apagar();
-        avion1.aterrizar();
+//        transporteTerrestre auto1 = new transporteTerrestre(categoriaTTerrestre.AUTO, 4,true,5);
+//        
+//        //medioTransporte m1 = new medioTransporte(true,8); -> las clases abstractas no pueden crear objetos
+//      
+//        auto1.encender();
+//        auto1.acelerar();
+//        auto1.conducir();
+//        auto1.frenar();
+//        auto1.apagar();
+//        
+//        transporteTerrestre bici1 = new transporteTerrestre(categoriaTTerrestre.BICICLETA, 2, false, 2);
+//        bici1.encender();
+//        bici1.acelerar();
+//        bici1.conducir();
+//        bici1.frenar();
+//        bici1.apagar();
+//        
+//        Avion avion1 = new Avion(2, 30);
+//        avion1.encender();
+//        avion1.acelerar();
+//        avion1.despegar();
+//        avion1.frenar();
+//        avion1.apagar();
+//        avion1.aterrizar();
                 
     }
     
---
+++ ClaseAbstracta/src/claseabstracta/TransporteAcuatico.java
@@ -0,0 +1,10 @@
+
+package claseabstracta;
+
+public class TransporteAcuatico extends medioTransporte{
+
+    public TransporteAcuatico(boolean deMotor, int cantidadPasajeros) {
+        super(deMotor, cantidadPasajeros);
+    }
+    
+}
